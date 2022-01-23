4f4bb9e38d0cc18d963952155fc4cd22f4c7f302
gholib <gtrandnet@gmail.com>
2022-01-23 12:25:03 +0500

    f

--- preload.py
+++ preload.py
@@ -5,10 +5,10 @@ f_token = open("files/token.txt")
 
 token = f_token.readline()
 bot = discum.Client(token=token)
-print("👀 scanning channels")
+print("scanning channels")
 
 f_channels = open("files/channels.txt", "r")
 channels = [line.rstrip() for line in f_channels]
 for channel_id in channels:
     parsingUsersID.parsingMembers(bot, channel_id)
-print(f'🌐 scanning end.')
+print('scanning end.')
